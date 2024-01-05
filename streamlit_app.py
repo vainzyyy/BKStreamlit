061e35c943624b26b5c81657761c857e5ad12ed3
vainzyyy <113153981+vainzyyy@users.noreply.github.com>
2024-01-05 20:21:10 +0700

    streamlitbaru_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,482 +1,74 @@
+# streamlit umumnya diinisialisasi dengan 'st'
 import streamlit as st
-import numpy as np
-import pandas as pd
-import re
-import itertools
 
-with open(dir, encoding='Latin1') as file:
-  lines =[line.strip() for line in file]
-
-lines[0:10]
-
-data = itertools.takewhile(
-  lambda x: len(x) == 76,
-  (' '.join(lines[i:(i + 10)]).split() for i in range(0, len(lines), 10))
+st.set_page_config(
+    page_title = "Estimasi CO2 Emissions",
+    page_icon = ':car: | :red_car:' #nama emoji 
 )
-df = pd.DataFrame.from_records(data)
-df.head()
 
-df.info()
-
-df = df.iloc[:,:-1]
-df = df.drop(df.columns[0], axis=1)
-
-df = df.astype(float)
-df.info()
-
-df.replace(-9.0, np.nan, inplace=True)
-
-df.isnull().sum()
-
-df.head()
-
-df.info()
-
-df_selected = df.iloc[:, [1, 2, 7,8,10,14,17,30,36,38,39,42,49,56]]
-df_selected.head()
-
-df_selected.info()
-
-column_mapping = {
-2: 'age',
-3: 'sex',
-8: 'cp',
-9: 'trestbps',
-11: 'chol',
-15: 'fbs',
-18: 'restecg',
-31: 'thalach',
-37: 'exang',
-39: 'oldpeak',
-40: 'slope',
-43: 'ca',
-50: 'thal',
-57: 'target'
-}
-df_selected.rename(columns=column_mapping, inplace=True)
-
-df_selected.info()
-
-df_selected.value_counts()
-
-df_selected.isnull().sum()
-
-columns_to_drop = ['ca', 'slope','thal']
-df_selected = df_selected.drop(columns_to_drop, axis=1)
-df_selected.isnull().sum()
-
-meanTBPS = df_selected['trestbps'].dropna()
-meanChol = df_selected['chol'].dropna()
-meanfbs = df_selected['fbs'].dropna()
-meanRestCG = df_selected['restecg'].dropna()
-meanthalach = df_selected['thalach'].dropna()
-meanexang = df_selected['exang'].dropna()
-meanTBPS = meanTBPS.astype(float)
-meanChol = meanChol.astype(float)
-meanfbs = meanfbs.astype(float)
-meanthalach = meanthalach.astype(float)
-meanexang = meanexang.astype(float)
-meanRestCG = meanRestCG.astype(float)
-meanTBPS = round(meanTBPS.mean())
-meanChol = round(meanChol.mean())
-meanfbs = round(meanfbs.mean())
-meanthalach = round(meanthalach.mean())
-meanexang = round(meanexang.mean())
-meanRestCG = round(meanRestCG.mean())
-
-fill_values = {'trestbps': meanTBPS, 'chol': meanChol, 'fbs': meanfbs,
-              'thalach':meanthalach,'exang':meanexang,'restecg':meanRestCG}
-dfClean = df_selected.fillna(value=fill_values)
-dfClean.info()
-
-dfClean.isnull().sum()
-
-duplicate_rows = dfClean.duplicated()
-dfClean[duplicate_rows]
-
-print("All Duplicate Rows:")
-dfClean[dfClean.duplicated(keep=False)]
-
-dfClean = dfClean.drop_duplicates()
-print("All Duplicate Rows:")
-dfClean[dfClean.duplicated(keep=False)]
-
-dfClean.head()
-
-dfClean['target'].value_counts()
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-dfClean.corr()
-
-cor_mat=dfClean.corr()
-fig,ax=plt.subplots(figsize=(15,10))
-sns.heatmap(cor_mat,annot=True,linewidths=0.5,fmt=".3f")
-
-dfClean.info()
-
-dfClean.head(5)
-
-X = dfClean.drop("target",axis=1).values
-y = dfClean.iloc[:,-1]
-
-dfClean['target'].value_counts().plot(kind='bar',figsize=(10,6),color=['green','blue'])
-plt.title("Count of the target")
-plt.xticks(rotation=0);
-
-from imblearn.over_sampling import SMOTE
-# oversampling
-smote = SMOTE(random_state=42)
-X_smote_resampled, y_smote_resampled = smote.fit_resample(X, y)
-plt.figure(figsize=(12, 4))
-new_df1 = pd.DataFrame(data=y)
-plt.subplot(1, 2, 1)
-new_df1.value_counts().plot(kind='bar',figsize=(10,6),color=['green','blue','red','yellow'])
-plt.title("target before over sampling with SMOTE ")
-plt.xticks(rotation=0);
-plt.subplot(1, 2, 2)
-new_df2 = pd.DataFrame(data=y_smote_resampled)
-new_df2.value_counts().plot(kind='bar',figsize=(10,6),color=['green','blue','red','yellow'])
-plt.title("target after over sampling with SMOTE")
-plt.xticks(rotation=0);
-plt.tight_layout()
-plt.show()
-
-new_df1 = pd.DataFrame(data=y)
-new_df1.value_counts()
-
-# over
-new_df2 = pd.DataFrame(data=y_smote_resampled)
-new_df2.value_counts()
-
-dfClean.describe()
-
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
-X_smote_resampled_normal = scaler.fit_transform(X_smote_resampled)
-len(X_smote_resampled_normal)
-
-dfcek1 = pd.DataFrame(X_smote_resampled_normal)
-dfcek1.describe()
-
-from sklearn.model_selection import train_test_split
-# membagi fitur dan target menjadi data train dan test (untuk yang oversample saja)
-X_train, X_test, y_train, y_test = train_test_split(X_smote_resampled, y_smote_resampled, test_size=0.2, random_state=42,stratify=y_smote_resampled)
-# membagi fitur dan target menjadi data train dan test (untuk yang oversample + normalization)
-X_train_normal, X_test_normal, y_train_normal, y_test_normal = train_test_split(X_smote_resampled_normal, y_smote_resampled, test_size=0.2, random_state=42,stratify = y_smote_resampled)
-
-from sklearn.metrics import accuracy_score,recall_score,f1_score,precision_score,roc_auc_score,confusion_matrix,precision_score
-def evaluation(Y_test,Y_pred):
-    acc = accuracy_score(Y_test,Y_pred)
-    rcl = recall_score(Y_test,Y_pred,average = 'weighted')
-    f1 = f1_score(Y_test,Y_pred,average = 'weighted')
-    ps = precision_score(Y_test,Y_pred,average = 'weighted')
-    metric_dict={'accuracy': round(acc,3),
-                  'recall': round(rcl,3),
-                  'F1 score': round(f1,3),
-                  'Precision score': round(ps,3)
-                }
-    return print(metric_dict)
-
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
-from sklearn.metrics import accuracy_score, classification_report
-
-knn_model = KNeighborsClassifier(n_neighbors = 3)
-knn_model.fit(X_train, y_train)
-
-y_pred_knn = knn_model.predict(X_test)
-# Evaluate the KNN model
-print("K-Nearest Neighbors (KNN) Model:")
-accuracy_knn_smote = round(accuracy_score(y_test,y_pred_knn),3)
-print("Accuracy:", accuracy_knn_smote)
-print("Classification Report:")
-print(classification_report(y_test, y_pred_knn))
-
-evaluation(y_test,y_pred_knn)
-
-cm = confusion_matrix(y_test, y_pred_knn)
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-plt.title('Confusion Matrix')
-plt.xlabel('True')
-plt.ylabel('Predict')
-plt.show()
-
-rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
-rf_model.fit(X_train, y_train)
-
-y_pred_rf = rf_model.predict(X_test)
-# Evaluate the Random Forest model
-print("\nRandom Forest Model:")
-accuracy_rf_smote = round(accuracy_score(y_test, y_pred_rf),3)
-print("Accuracy:",accuracy_rf_smote)
-print("Classification Report:")
-print(classification_report(y_test, y_pred_rf))
-
-evaluation(y_test,y_pred_rf)
-
-cm = confusion_matrix(y_test, y_pred_rf)
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-plt.title('Confusion Matrix')
-plt.xlabel('True')
-plt.ylabel('Predict')
-plt.show()
-
-xgb_model = XGBClassifier(learning_rate=0.1, n_estimators=100, random_state=42)
-xgb_model.fit(X_train, y_train)
-
-y_pred_xgb = xgb_model.predict(X_test)
-# Evaluate the XGBoost model
-print("\nXGBoost Model:")
-accuracy_xgb_smote = round(accuracy_score(y_test, y_pred_xgb),3)
-print("Accuracy:",accuracy_xgb_smote)
-print("Classification Report:")
-print(classification_report(y_test, y_pred_xgb))
-
-evaluation(y_test,y_pred_xgb)
-
-cm = confusion_matrix(y_test, y_pred_xgb)
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-plt.title('Confusion Matrix')
-plt.xlabel('True')
-plt.ylabel('Predict')
-plt.show()
-
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
-from sklearn.metrics import accuracy_score, classification_report
-
-knn_model = KNeighborsClassifier(n_neighbors=3)
-knn_model.fit(X_train_normal, y_train_normal)
-
-y_pred_knn = knn_model.predict(X_test_normal)
-# Evaluate the KNN model
-print("K-Nearest Neighbors (KNN) Model:")
-accuracy_knn_smote_normal = round(accuracy_score(y_test_normal,y_pred_knn),3)
-print("Accuracy:", accuracy_knn_smote_normal)
-print("Classification Report:")
-print(classification_report(y_test_normal, y_pred_knn))
-
-evaluation(y_test_normal,y_pred_knn)
-
-cm = confusion_matrix(y_test_normal, y_pred_knn)
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-plt.title('Confusion Matrix')
-plt.xlabel('True')
-plt.ylabel('Predict')
-plt.show()
-
-rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
-rf_model.fit(X_train_normal, y_train_normal)
-
-y_pred_rf = rf_model.predict(X_test_normal)
-# Evaluate the Random Forest model
-print("\nRandom Forest Model:")
-accuracy_rf_smote_normal = round(accuracy_score(y_test_normal, y_pred_rf),3)
-print("Accuracy:",accuracy_rf_smote_normal )
-print("Classification Report:")
-print(classification_report(y_test_normal, y_pred_rf))
-
-evaluation(y_test_normal,y_pred_rf)
-
-cm = confusion_matrix(y_test_normal, y_pred_rf)
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-plt.title('Confusion Matrix')
-plt.xlabel('True')
-plt.ylabel('Predict')
-plt.show()
-
-xgb_model = XGBClassifier(learning_rate=0.1, n_estimators=100, random_state=42)
-xgb_model.fit(X_train_normal, y_train_normal)
-
-y_pred_xgb = xgb_model.predict(X_test_normal)
-# Evaluate the XGBoost model
-print("\nXGBoost Model:")
-accuracy_xgb_smote_normal = round(accuracy_score(y_test_normal, y_pred_xgb),3)
-print("Accuracy:",accuracy_xgb_smote_normal)
-print("Classification Report:")
-print(classification_report(y_test_normal, y_pred_xgb))
-
-evaluation(y_test_normal,y_pred_xgb)
-
-cm = confusion_matrix(y_test_normal, y_pred_xgb)
-plt.figuacre(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-plt.title('Confusion Matrix')
-plt.xlabel('True')
-plt.ylabel('Predict')
-plt.show()
-
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.model_selection import RandomizedSearchCV
-
-knn_model = KNeighborsClassifier()
-param_grid = {
-    "n_neighbors": range(3, 21),
-    "metric": ["euclidean", "manhattan", "chebyshev"],
-    "weights": ["uniform", "distance"],
-    "algorithm": ["auto", "ball_tree", "kd_tree"],
-    "leaf_size": range(10, 61),
-}
-knn_model = RandomizedSearchCV(estimator=knn_model, param_distributions=param_grid, n_iter=100, scoring="accuracy", cv=5)
-knn_model.fit(X_train_normal, y_train_normal)
-best_params = knn_model.best_params_
-print(f"Best parameters: {best_params}")
-
-y_pred_knn = knn_model.predict(X_test_normal)
-# Evaluate the KNN model
-print("K-Nearest Neighbors (KNN) Model:")
-accuracy_knn_smote_normal_Tun = round(accuracy_score(y_test_normal,y_pred_knn),3)
-print("Accuracy:", accuracy_knn_smote_normal_Tun)
-print("Classification Report:")
-print(classification_report(y_test_normal, y_pred_knn))
-
-evaluation(y_test_normal,y_pred_knn)
-
-
-rf_model = RandomForestClassifier()
-param_grid = {
-    "n_estimators": [100, 200],
-    "max_depth": [ 10, 15],
-    "min_samples_leaf": [1, 2],
-    "min_samples_split": [2, 5],
-    "max_features": ["sqrt", "log2"],
-    # "random_state": [42, 100, 200]
-}
-rf_model = RandomizedSearchCV(rf_model, param_grid, n_iter=100, cv=5, n_jobs=-1)
-rf_model.fit(X_train_normal, y_train_normal)
-best_params = rf_model.best_params_
-print(f"Best parameters: {best_params}")
-
-y_pred_rf = rf_model.predict(X_test_normal)
-# Evaluate the Random Forest model
-print("\nRandom Forest Model:")
-accuracy_rf_smote_normal_Tun = round(accuracy_score(y_test_normal, y_pred_rf),3)
-print("Accuracy:",accuracy_rf_smote_normal_Tun)
-print("Classification Report:")
-print(classification_report(y_test_normal, y_pred_rf))
-
-evaluation(y_test_normal,y_pred_rf)
-
-cm = confusion_matrix(y_test_normal, y_pred_knn)
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-plt.title('Confusion Matrix')
-plt.xlabel('True')
-plt.ylabel('Predict')
-plt.show()
-
-xgb_model = XGBClassifier()
-param_grid = {
-    "max_depth": [3, 5, 7],
-    "learning_rate": [0.01, 0.1],
-    "n_estimators": [100, 200],
-    "gamma": [0, 0.1],
-    "colsample_bytree": [0.7, 0.8],
-}
-xgb_model = RandomizedSearchCV(xgb_model, param_grid, n_iter=10, cv=5, n_jobs=-1)
-xgb_model.fit(X_train_normal, y_train_normal)
-best_params = xgb_model.best_params_
-print(f"Best parameters: {best_params}")
-
-y_pred_xgb = xgb_model.predict(X_test_normal)
-# Evaluate the XGBoost model
-print("\nXGBoost Model:")
-accuracy_xgb_smote_normal_Tun = round(accuracy_score(y_test_normal, y_pred_xgb),3)
-print("Accuracy:",accuracy_xgb_smote_normal_Tun)
-print("Classification Report:")
-print(classification_report(y_test_normal, y_pred_xgb))
-
-evaluation(y_test_normal,y_pred_xgb)
-
-
-cm = confusion_matrix(y_test_normal, y_pred_xgb)
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-plt.title('Confusion Matrix')
-plt.xlabel('True')
-plt.ylabel('Predict')
-plt.show()
-
-import matplotlib.pyplot as plt
-model_comp1 = pd.DataFrame({'Model': ['K-Nearest Neighbour','Random Forest',
-                    'XGBoost'], 'Accuracy': [accuracy_knn_smote*100,
-                    accuracy_rf_smote*100,accuracy_xgb_smote*100]})
-
-model_comp1.head()
-
-# Membuat bar plot dengan keterangan jumlah
-fig, ax = plt.subplots()
-bars = plt.bar(model_comp1['Model'], model_comp1['Accuracy'], color=['red', 'green', 'blue'])
-plt.xlabel('Model')
-plt.ylabel('Accuracy (%)')
-plt.title('Oversample')
-plt.xticks(rotation=45, ha='right') # Untuk memutar label sumbu x agar lebih mudah dibaca
-# Menambahkan keterangan jumlah di atas setiap bar
-for bar in bars:
-    yval = bar.get_height()
-    plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), ha='center', va='bottom')
-plt.show()
-
-model_comp2 = pd.DataFrame({'Model': ['K-Nearest Neighbour','Random Forest',
-                      'XGBoost'], 'Accuracy': [accuracy_knn_smote_normal*100,
-                      accuracy_rf_smote_normal*100,accuracy_xgb_smote_normal*100]})
-
-model_comp2.head()
-
-# Membuat bar plot dengan keterangan jumlah
-fig, ax = plt.subplots()
-bars = plt.bar(model_comp2['Model'], model_comp2['Accuracy'], color=['red', 'green', 'blue'])
-plt.xlabel('Model')
-plt.ylabel('Accuracy (%)')
-plt.title('Normalization + Oversampling')
-plt.xticks(rotation=45, ha='right') # Untuk memutar label sumbu x agar lebih mudah dibaca
-# Menambahkan keterangan jumlah di atas setiap bar
-for bar in bars:
-    yval = bar.get_height()
-    plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), ha='center', va='bottom')
-plt.show()
-
-model_comp3 = pd.DataFrame({'Model': ['K-Nearest Neighbour','Random Forest',
-                    'XGBoost'], 'Accuracy': [accuracy_knn_smote_normal_Tun*100,
-                    accuracy_rf_smote_normal_Tun*100,accuracy_xgb_smote_normal_Tun*100]})
-
-model_comp3.head()
-
-# Membuat bar plot dengan keterangan jumlah
-fig, ax = plt.subplots()
-bars = plt.bar(model_comp3['Model'], model_comp3['Accuracy'], color=['red', 'green', 'blue'])
-plt.xlabel('Model')
-plt.ylabel('Accuracy (%)')
-plt.title('Normalization + Oversampling + Tunning')
-plt.xticks(rotation=45, ha='right') # Untuk memutar label sumbu x agar lebih mudah dibaca
-# Menambahkan keterangan jumlah di atas setiap bar
-for bar in bars:
-    yval = bar.get_height()
-    plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), ha='center', va='bottom')
-plt.show()
-
-# Membuat bar plot dengan keterangan jumlah
-fig, ax = plt.subplots()
-bars = plt.bar(model_compBest['Model'], model_compBest['Accuracy'], color=['red', 'green', 'blue'])
-plt.xlabel('Model')
-plt.ylabel('Accuracy (%)')
-plt.title('Best Model Comparison')
-plt.xticks(rotation=45, ha='right') # Untuk memutar label sumbu x agar lebih mudah dibaca
-# Menambahkan keterangan jumlah di atas setiap bar
-for bar in bars:
-    yval = bar.get_height()
-    plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), ha='center', va='bottom')
-plt.show()
+# st.write dapat digunakan menampilkan test,dataframe,visualisasi
+st.title('Estimasi CO2 Emissions')
+st.write('information about Co2 Emission')
+
+
+# st.sidebar dapat digunakan untuk membuat sidebar
+st.sidebar.header("User Input Features")
+
+def user_input_features():
+    EngineSize = st.sidebar.number_input('Insert a Engine Size', 1.0 , 8.4) #(label,minvalues,maxvalues,initial values)
+    Cylinders = st.sidebar.number_input('Insert a Cylinders', 3 , 16)
+    FuelConsumptionTotal = st.sidebar.number_input('Insert a Total Fuel Comsumption (L/100 km)', 1.0 , 25.8)
+    
+    data = {'ENGINESIZE': EngineSize,
+            'CYLINDERS': Cylinders,
+            'FUELCONSUMPTION_COMB': FuelConsumptionTotal
+            }
+    
+    features = pd.DataFrame(data,index=[0])
+    return features
+
+df = user_input_features()
+st.header("User Input Features")
+st.write(df)
+
+dfModel = pd.read_csv('data/FuelConsumption.csv')
+dfModel = dfModel[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+
+df2 = dfModel.iloc[:,:-1]
+if df.columns.equals(df2.columns):
+    st.write("Column names are the same.")
+else:
+    st.write("Column names are different.")
+
+# separate the feature and target
+X = dfModel.iloc[:,:-1]
+Y = dfModel.iloc[:,-1]
+
+# splitting data into data train and test
+x_train,x_test,y_train,y_test = train_test_split(X,Y,test_size = 0.3 ,random_state = 42)
+
+# creating model
+LinearReg = LinearRegression()
+LinearReg.fit(x_train,y_train)
+
+st.write('press button below to predict : ')
+if st.button('Predict'):
+    # create progres bar widget with initial progress is 0%
+    bar = st.progress(0)
+        # create an empty container or space
+    status_text = st.empty()
+    for i in range(1,101):
+        # create a text to showing a percentage process
+        status_text.text("%i%% complete" %i)
+        # give bar progress values
+        bar.progress(i)
+        # give bar progress time to execute the values
+        time.sleep(0.01)
+
+    ypred = LinearReg.predict(df)
+    ypred = float(ypred[0])
+    ypred = "{:.2f}".format(ypred)
+
+    st.subheader('Prediction')
+    st.metric('CO2 Emission',ypred,' g/km')
 
