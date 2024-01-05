e1bdc972d4fefc865d7b27bbe18d7de8257bb708
vainzyyy <113153981+vainzyyy@users.noreply.github.com>
2024-01-05 19:54:14 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,24 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import re
+import itertools
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+!ls "/content/drive/My Drive/Dataset/hungarian.data"
+
+dir = '/content/drive/My Drive/Dataset/hungarian.data'
+
+with open(dir, encoding='Latin1') as file:
+  lines =[line.strip() for line in file]
+
+lines[0:10]
+
+data = itertools.takewhile(
+  lambda x: len(x) == 76,
+  (' '.join(lines[i:(i + 10)]).split() for i in range(0, len(lines), 10))
+)
+df = pd.DataFrame.from_records(data)
+df.head()
